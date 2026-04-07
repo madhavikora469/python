c78838503058010de13de4b32443ca31d6b43590
madhavikora469 <madhavikora6@gmail.com>
2026-04-07 18:04:02 +0530

    Add files via upload

---
+++ class srting (1).py
@@ -0,0 +1,7 @@
+class namedisplay:
+    def __init  __ (self,name):
+        self.name=name
+    def __   str __  (self):
+       return "student name is: "+self.name
+nd=namedisplay("Raju")
+print(nd)
